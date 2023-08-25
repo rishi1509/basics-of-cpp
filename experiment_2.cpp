f4978ae03baf02256231011c29f1861c3204b851
rishi1509 <139452304+rishi1509@users.noreply.github.com>
2023-08-25 10:28:37 +0530

    Update experiment_2.cpp

--- experiment_2.cpp
+++ experiment_2.cpp
@@ -52,3 +52,19 @@ int main()
     return 0;
 }
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool imagecolor = true;
+    if(imagecolor)
+      cout<<"yes the image is colourful"<<endl;
+    else
+      cout<<"image is black and white"<<endl;
+
+    
+}
+/*OUTPUT
+yes the image is colourful
+*/
+
